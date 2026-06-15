4bbce184f10920a6ba3115321515aff30ee1454a
Philippe Debeaumont <60342297+philippedebeaumont@users.noreply.github.com>
2026-06-15 19:40:14 +0200

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l, r = 0, 0
+        longest = 0
+        frequency = [0] * 26
+
+        for r in range(len(s)):
+            frequency[ord(s[r]) - ord("A")] += 1
+
+            if r - l >= max(frequency) + k:
+                frequency[ord(s[l]) - ord("A")] -= 1
+                l += 1
+            else:
+                r += 1
+                longest = r - l                
+        
+        return longest
