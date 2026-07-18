3be4d148bc4a4d2d84a62f84bc50fde99166779b
Philippe Debeaumont <60342297+philippedebeaumont@users.noreply.github.com>
2026-07-18 12:37:57 +0200

    Add: course-schedule - submission-7

---
+++ Data Structures & Algorithms/course-schedule/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj_list = {i: [] for i in range(numCourses)}
+        for course, prereq in prerequisites:
+            adj_list[course].append(prereq)
+            
+        visiting = set()
+        visited = set()
+        
+        def has_cycle(course):
+            if course in visiting:
+                return True
+            if course in visited:
+                return False
+                
+            visiting.add(course)
+            for prereq in adj_list[course]:
+                if has_cycle(prereq):
+                    return True
+            visiting.remove(course)
+            visited.add(course)
+            return False
+            
+        # Check every course to handle disconnected components
+        for course in range(numCourses):
+            if has_cycle(course):
+                return False
+        return True
