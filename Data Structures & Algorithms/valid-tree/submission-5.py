7878433d2acad85ca5a6f2649bab680975490044
Philippe Debeaumont <60342297+philippedebeaumont@users.noreply.github.com>
2026-07-18 12:58:33 +0200

    Add: valid-tree - submission-5

---
+++ Data Structures & Algorithms/valid-tree/submission-5.py
@@ -0,0 +1,31 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n-1:
+            return False
+
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        visiting = set()
+
+        def has_cycle(node, parent):
+            if node in visiting:
+                return True
+            
+            visiting.add(node)
+            if adj[node]:
+                for neighbor in adj[node]:
+                    if neighbor == parent:
+                        continue
+                    if has_cycle(neighbor, node):
+                        return True
+            visiting.remove(node)
+
+            return False
+
+        for i in range(n):
+            if has_cycle(i, -1):
+                return False
+        return True
