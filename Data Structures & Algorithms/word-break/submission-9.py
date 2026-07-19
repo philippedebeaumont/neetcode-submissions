18820800477ed1716eabe4669da979bdafdec0f1
Philippe Debeaumont <60342297+philippedebeaumont@users.noreply.github.com>
2026-07-19 20:21:09 +0200

    Add: word-break - submission-9

---
+++ Data Structures & Algorithms/word-break/submission-9.py
@@ -0,0 +1,34 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        root = current = {}
+        for word in wordDict:
+            current = root
+            for c in word:
+                if c not in current:
+                    current[c] = {}
+                current = current[c]
+            current["#"] = True
+        
+        memo = {}
+        
+        def dfs(i):
+            if i == len(s):
+                memo[i] = True
+                return True
+
+            if i in memo:
+                return memo[i]
+
+            current = root
+            for j in range(i, len(s)):
+                if s[j] not in current:
+                    break
+                current = current[s[j]]
+                if "#" in current:
+                    if dfs(j+1):
+                        return True
+
+            memo[i] = False
+            return False
+
+        return dfs(0)
